17ec7cc8ad8d3afc3241ec76d5aab0137ed194a1
Kintaro-Oe <marcusclairmont@gmail.com>
2019-05-05 19:05:36 +0100

    add a jQuery AJAX to retrieve weather by city from a weather API. add city section to HTML

--- Jasmin/src/jqueryIndexCode.js
+++ Jasmin/src/jqueryIndexCode.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+
+  $('#select-city').change(function() {
+    var city = $('#select-city').val();
+    if(city === "") { return; };
+    displayWeather(city);
+  })
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?id=';
+    var key = '&appid=7f2adfbb35a4ee131c7b1ca64d0040e2';
+    var units = '&units=metric'
+    $.get(url + city + units + key, function(data) {
+      $('#current-temperature').text(data.main.temp)
+    });
+  };
+
   var thermostat = new Thermostat();
   updateTemperature();
   $('#power-saving-status').text('on')
@@ -32,6 +48,6 @@ $(document).ready(function() {
   function updateTemperature() {
     $('#temperature').text(thermostat.getCurrentTemp());
     $('#temperature').attr('class', thermostat.energyUsage());
-
   };
+
 });
